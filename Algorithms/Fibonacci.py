e32d07d7513312e0382900daafbd5b241873c688
perchonock <me_on@mail.ru>
2015-11-11 12:28:28 +0300

    fibonacci numbers (finished)

--- Algorithms/Fibonacci.py
+++ Algorithms/Fibonacci.py
@@ -1,8 +1,19 @@
-import sys
-#input0 = sys.stdin.read()
-input0 = "5"
+input0 = "3"
 input = int(input0)
 
-fibonacci_nums = [0,1]
+def get_fib_num(n):
+    fibonacci_nums = [0,1]
+    i = 2
+    while len(fibonacci_nums) <= n:
+        current_num = fibonacci_nums[i-1] + fibonacci_nums[i-2]
+        print(current_num)
+        fibonacci_nums.append(current_num)
+        print(fibonacci_nums)
+        i += 1
+    return fibonacci_nums[n]
+
+print(get_fib_num(input))
+
+
 
 

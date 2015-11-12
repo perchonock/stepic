4adb14c839f15b307ed56c8dfb12bb318e77ca1b
perchonock <me_on@mail.ru>
2015-11-12 18:54:04 +0300

    2 more problems
    def last_figure(n)
    def fib_mod(n,m)

--- Algorithms/Fibonacci.py
+++ Algorithms/Fibonacci.py
@@ -1,19 +1,37 @@
-input0 = "3"
-input = int(input0)
-
-def get_fib_num(n):
+def fib_num(n):
     fibonacci_nums = [0,1]
     i = 2
     while len(fibonacci_nums) <= n:
         current_num = fibonacci_nums[i-1] + fibonacci_nums[i-2]
-        print(current_num)
         fibonacci_nums.append(current_num)
-        print(fibonacci_nums)
         i += 1
     return fibonacci_nums[n]
 
-print(get_fib_num(input))
+def last_figure(n):
+    last_figures = [0,1]
+    for i in range(n-1):
+        current_num_figure = (last_figures[0] + last_figures[1])%10
+        last_figures.append(current_num_figure)
+        last_figures.pop(0)
+    return last_figures[1]
 
+def fib_mod(n,m):
+    mods = [0,1]
+    k=2
+    if n > 1:
+        for i in range(2,n+1):
+            current_num_mod = (mods[i-1] + mods[i-2])%m
+            mods.append(current_num_mod)
+            if mods[i-1] == 0 and mods[i] == 1:
+                return mods[(n%(k))]
+            k=i
+        else:
+            return mods[-1]
+n=2345
+m=8
+print(fib_mod(n,m))
+
+print(fib_num(n)%m)
 
 
 

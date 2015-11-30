f237b6154e22ab1d0177dd10f06efcec6f1fdf80
perchonock <me_on@mail.ru>
2015-11-30 16:50:38 +0300

    binary search
    Passes TL

--- Algorithms/binary_search.py
+++ Algorithms/binary_search.py
@@ -1,33 +1,31 @@
-__author__ = 'YKolotolova'
-
 import sys
-#input = sys.stdin.read()
-input = "5 1 5 8 12 13\n5 8 1 23 1 11"
-input = input.splitlines()
+#input = sys.stdin.read().splitlines()
+input = "5 1 5 8 12 13\n5 8 1 23 1 11".splitlines()
 array = [int(n) for n in input[0].split()]
 searched_numbers = [int(n) for n in input[1].split()]
 length=array[0]
-#print(input[0])
-#print(input[1])
+array = array[1:]
 
 def binary_search(number,array):
-    if number > array[-1] or number < array[0]:
-        return -1
     left_limit = 0
-    right_limit = length
+    right_limit = length-1
     while left_limit <= right_limit:
-        position = int((left_limit+right_limit)/2)
+        position = (left_limit+right_limit)//2
         if array[position] == number:
-            return position +1
+            return position + 1
         elif array[position] > number:
             right_limit = position-1
-        elif array[position] < number:
+        else:
             left_limit = position+1
-    return - 1
+    return -1
+
+# for number in range(1,len(searched_numbers)):
+#     print(binary_search(searched_numbers[number], array), end = " ")
 
 index_array = []
 for number in range(1,len(searched_numbers)):
-
-    index_array.append(binary_search(searched_numbers[number], array[1:]))
-
+    index_array.append(binary_search(searched_numbers[number], array))
 print(" ".join(map(str,index_array)))
+
+
+

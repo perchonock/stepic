600b50c1a622b953795672fe90da1a2a7228a0bf
perchonock <me_on@mail.ru>
2015-12-11 15:01:47 +0300

    Working KnapSack

--- Algorithms/KnapSack_DynamicProgramming.py
+++ Algorithms/KnapSack_DynamicProgramming.py
@@ -1,16 +1,28 @@
-W = 10
-n = 4
-weights = [0,6,3,4,2]
-c = [0,30,14,16,9]
+# W = 10
+# n = 3
+# weights = [0,1,4,8]
 
-D = [[0]*(W+1)]*(n+1)
+#D = [[0]*(W+1)]*(n+1)
 
-for i in range(1,n+1):
-    for w in range(1,W+1):
-        D[i][w] = D[i-1][w]
-        if weights[i] <= w:
-           D[i][w] = max(D[i][w],D[i-1][w - weights[i]] + c[i])
+def KnapSack(W,n,weights):
+    D = []
+    for n in range(0,n+1):
+            tmp = [0 for k in range(0,W+1)]
+            D.extend([tmp])
 
-for line in D:
-    print(line)
-print(D[n][W])
+    for i in range(1,n+1):
+        for w in range(1,W+1):
+            D[i][w] = D[i-1][w]
+            if weights[i] <= w:
+                D[i][w] = max(D[i][w],D[i-1][w - weights[i]] + weights[i])
+    return(D[n][W])
+
+import sys
+#input = sys.stdin.read().splitlines()
+input = "10 3\n1 4 8".splitlines()
+W = int(input[0].split()[0])
+n = int(input[0].split()[1])
+weights = [int(i) for i in input[1].split()]
+weights.insert(0,0)
+
+print(KnapSack(W,n,weights))

3eae2b1410c9d57b512772a06673f7e1024f4a08
perchonock <me_on@mail.ru>
2016-04-25 18:34:56 +0300

    Class Inheritance

--- python_application/ClassInheritance.py
+++ python_application/ClassInheritance.py
@@ -1,39 +1,45 @@
 classes = {}
+nodes = []
 
 n = int(input())
 for i in range(n):
     str = input()
+    nodes.extend(str.split())
     if ":" in str:
         lst = str.split(':')
         classes[lst[0].strip()] = lst[1].split()
+
     else:
         classes[str] = []
 
+
 def isparent(parent,child):
+    if child not in nodes or parent not in nodes:
+        print('No')
+        return
     if child == parent:
         print('Yes')
-
     elif child in classes:
         if parent in classes[child]:
             print("Yes")
 
         else:
-            for child1, parents in classes.items():
-                if parent in parents:
-                    isparent(child1,child)
+            for cl in classes[child]:
+                if cl in classes:
+                    isparent(parent,cl)
                     return
             print('No')
-
     else:
-        print('No')
+        print("No")
 
-
-print(classes)
+#print(classes)
 
 k = int(input())
 for i in range(k):
     lst = input().split()
     isparent(lst[0],lst[1])
+
+
 
 
 

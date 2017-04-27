5c3f3b9fd989461fdc98d6a3e9d35ce0fb11119e
perchonock <me_on@mail.ru>
2017-04-27 19:13:44 +0300

    max element in window: done

--- Algorithms2/maxInWindow.py
+++ Algorithms2/maxInWindow.py
@@ -1,7 +1,6 @@
 #import sys
 #input = sys.stdin.read()
 
-
 def getMaxNaive(input):
     n = int(input.split('\n')[0])
     nums = [int(i) for i in input.split('\n')[1].split()]
@@ -11,12 +10,107 @@ def getMaxNaive(input):
     for i in range(r):
         m = max(nums[i:(i+w)])
         result += str(m) + ' '
-
     return result.strip()
+
+class StackWithMaxElement:
+    def __init__(self):
+        self.items = []
+        self.max = []
+
+    def push(self, item):
+        self.items.append(int(item))
+        if len(self.max) == 0:
+            self.max.append(item)
+        else:
+            if item > self.max[-1]:
+                self.max.append(item)
+            else:
+                last = self.max[-1]
+                self.max.append(last)
+
+    def pop(self):
+        self.max.pop()
+        return self.items.pop()
+
+    def print(self):
+        print(self.items)
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def get_max(self):
+        return self.max[-1]
+
+    def isEmpty(self):
+        return self.items == []
+
+class QueueWithMaxElement:
+    def __init__(self):
+        self.stack1 = StackWithMaxElement()
+        self.stack2 = StackWithMaxElement()
+
+    def pushBack(self, item):
+        self.stack1.push(item)
+
+    def popFront(self):
+        if self.stack2.isEmpty():
+            while len(self.stack1.items) != 0:
+                self.stack2.push(self.stack1.pop())
+        return self.stack2.pop()
+
+    def get_max(self):
+        if(self.stack1.isEmpty()):
+            return self.stack2.get_max()
+        elif(self.stack2.isEmpty()):
+            return self.stack1.get_max()
+        else:
+            return max(self.stack1.get_max(), self.stack2.get_max())
+
+    def print(self):
+        print('stack1', self.stack1.items)
+        print('max', self.stack1.max)
+        print('stack2', self.stack2.items)
+        print('max', self.stack2.max)
 
 def getMax(input):
     n = int(input.split('\n')[0])
     nums = [int(i) for i in input.split('\n')[1].split()]
     w = int(input.split('\n')[2])
+    qu = QueueWithMaxElement()
+    for i in range(w):
+        qu.pushBack(nums[i])
+    result = str(qu.get_max()) + ' '
+    for j in range(w,n):
+        qu.popFront()
+        qu.pushBack(nums[j])
+        result += str(qu.get_max()) + ' '
+
+    return result.strip()
 
 #sys.stdout.write(getMax(input))
+
+# q = QueueWithMaxElement()
+# q.pushBack(2)
+# q.pushBack(7)
+# q.pushBack(3)
+# q.pushBack(1)
+# q.print()
+# print(q.get_max())
+# q.popFront()
+# q.pushBack(5)
+# q.print()
+# print(q.get_max())
+# q.popFront()
+# q.pushBack(2)
+# q.print()
+# print(q.get_max())
+# q.popFront()
+# q.pushBack(6)
+# q.print()
+# print(q.get_max())
+# q.popFront()
+# q.pushBack(2)
+# q.print()
+# print(q.get_max())
+
+#print(getMax('8\n2 7 3 1 5 2 6 2\n4'))
--- Algorithms2/test_maxInWindow.py
+++ Algorithms2/test_maxInWindow.py
@@ -23,3 +23,27 @@ class TestMaxInWindow(TestCase):
         output = getMax(input)
         self.assertTrue(output == '9', output)
 
+    def test4(self):
+        input = '15\n73 65 24 14 44 20 65 97 27 6 42 1 6 41 16\n7'
+        output = getMax(input)
+        self.assertTrue(output == '73 97 97 97 97 97 97 97 42', output)
+
+    def test5(self):
+        input = '15\n28 7 64 40 68 86 80 93 4 53 32 56 68 18 59\n12'
+        output = getMax(input)
+        self.assertTrue(output == '93 93 93 93', output)
+
+    def test6(self):
+        input = '15\n16 79 20 19 43 72 78 33 40 52 70 79 66 43 60\n12'
+        output = getMax(input)
+        self.assertTrue(output == '79 79 79 79', output)
+
+    def test7(self):
+        input = '15\n34 51 61 90 26 84 2 25 7 8 25 78 21 47 25\n3'
+        output = getMax(input)
+        self.assertTrue(output == '61 90 90 90 84 84 25 25 25 78 78 78 47', output)
+
+    def test8(self):
+        input = '15\n27 83 29 77 6 3 48 2 16 72 46 38 55 2 58 \n5'
+        output = getMax(input)
+        self.assertTrue(output == '83 83 77 77 48 72 72 72 72 72 58', output)
